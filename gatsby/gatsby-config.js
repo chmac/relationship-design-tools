4355625b81edbd52cbe8112badd6b1962737bcad
Callum Macdonald <git@callum-macdonald.com>
2019-09-28 21:18:26 +0200

    Try to import markdown.

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -2,19 +2,48 @@ const path = require("path");
 
 module.exports = {
   siteMetadata: {
-    title: "Ueno Gatsby Starter"
+    title: "Relationship Design Tools"
   },
   plugins: [
-    "gatsby-plugin-ueno",
+    // "gatsby-plugin-ueno",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-typescript",
     "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/../content/`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
+          },
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants"
+        ]
+      }
+    },
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "ueno-gatsby-starter",
-        short_name: "starter",
+        name: "relationship-design-tools",
+        short_name: "rdt",
         start_url: "/",
         background_color: "#663399",
         theme_color: "#663399",
