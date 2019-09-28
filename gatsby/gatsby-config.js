ea1197d4d2d42422f11aac87fdff7cf2c0159a72
Callum Macdonald <git@callum-macdonald.com>
2019-09-28 20:51:11 +0200

    Prettier.

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,40 +1,40 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   siteMetadata: {
-    title: 'Ueno Gatsby Starter',
+    title: "Ueno Gatsby Starter"
   },
   plugins: [
-    'gatsby-plugin-ueno',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-ueno",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'ueno-gatsby-starter',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/favicon.png',
-      },
+        name: "ueno-gatsby-starter",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/assets/images/favicon.png"
+      }
     },
     {
-      resolve: 'gatsby-plugin-react-svg',
+      resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
-          include: path.resolve(__dirname, 'src/assets/svg'),
-        },
-      },
+          include: path.resolve(__dirname, "src/assets/svg")
+        }
+      }
     },
     {
-      resolve: 'gatsby-plugin-layout',
+      resolve: "gatsby-plugin-layout",
       options: {
-        component: require.resolve('./src/components/app-layout/AppLayout.tsx'),
-      },
-    },
-  ],
-}
+        component: require.resolve("./src/components/app-layout/AppLayout.tsx")
+      }
+    }
+  ]
+};
